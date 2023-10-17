07ce3644caa4dc58adecd46a63e4dd1a1cf1955c
Per-Olof Bengtsson <po@niffic.se>
2023-10-17 16:44:18 +0200

    cleaned up the gamemap test and code

--- gamemap.test.js
+++ gamemap.test.js
@@ -1,21 +1,34 @@
 import { GameMap } from './gamemap'
+import { Tile } from './tile'
+import { jest } from '@jest/globals'
 
 describe('GameMap', () => {
+  let gamemap
+  let mockNext
+  beforeEach(() => {
+    mockNext = jest.fn().mockReturnValue({ value: new Tile('asd') })
+    gamemap = new GameMap({ width: 10, height: 10 }, { next: mockNext })
+  })
   describe('when created', () => {
     it('has given width', () => {
-      expect(new GameMap({ width: 10, height: 10 }).width).toEqual(10)
+      expect(gamemap.width).toEqual(10)
     })
     it('has given height', () => {
-      expect(new GameMap({width: 15, height: 15 }).height).toEqual(15)
+      expect(gamemap.height).toEqual(10)
     })
-    it('has list of tiles with widthn length', () => {
-      expect(new GameMap({width: 15, height: 15 }).tiles).toHaveLength(15)
+    it('has list of tiles with width length', () => {
+      expect(gamemap.tiles).toHaveLength(10)
     })
-    it('has lists of lists where each sublist is 15 items', () => {
-      const gameMap = new GameMap({ width: 15, height: 15 })
-      const lengths = gameMap.tiles.map((a) => a.length)
-      const allCorrectLength = lengths.every((l) => l === 15)
-      expect(allCorrectLength).toEqual(true)
+    it('has 100 map tiles', () => {
+      const allTiles = gamemap.tiles.flatMap((x) => x)
+      expect(allTiles).toHaveLength(100)
+    })
+    it('has used the generator to get all tiles', () => {
+      expect(mockNext).toHaveBeenCalledTimes(100)
+    })
+    it('has only tile objects in the map', () => {
+      const allTiles = gamemap.tiles.flatMap((x) => x)
+      expect(allTiles.every((tile) => tile instanceof Tile)).toEqual(true)
     })
   })
 })
